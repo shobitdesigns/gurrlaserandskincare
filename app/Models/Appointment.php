d49c28c0effe3abe0a62510b4f4126e7a19815e3
Avinash <avinash.ukuu@gmail.com>
2025-01-31 16:37:04 +0530

    modifications

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Appointment extends Model
 {
     use HasFactory;
+
+    protected $guarded  =   ['id'];
+    protected $table    =   'appointments';
+
+    public function getTimeAttribute()
+    {
+        return Carbon::parse($this->attributes['time'])->format('g:i A');
+    }
 }
